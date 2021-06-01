2b832edb8f7a5dbb19399004fa265cb3b5def514
Vijayakumar Ravi <vijayravi2998@gmail.com>
2021-06-01 10:27:10 +0530

    updated to

--- update-repo.sh
+++ update-repo.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 
 # sudo rm -r temp/
-mkdir temp && cd temp
+mkdir ~/Git/arch-repo/temp
 
 update-yay() {
+	rm ~/Git/arch-repo/x86_64/yay*
+	cd ~/Git/arch-repo/temp/
 	git clone https://aur.archlinux.org/yay.git
 	cd yay
 	makepkg -s
-	mv -v *.pkg.tar.zst ../../x86_64/.
+	mv -v *.pkg.tar.zst ~/Git/arch-repo/x86_64/
+}
+
+git-push() {
+	cd ~/Git/arch-repo/
+	$ver = curl -sfLS 'https://api.github.com/repos/Jguer/yay/releases/latest' | grep 'browser_download_url' | tail -1 | cut -d '"' -f 4 | cut -d/ -f2- | awk -F'/' '{print $7}'
+	git status
+	sleep 5
+	git add .
+	git commit -m "updated to $ver"
+	git push
 }
 
 update-yay
+git-push
 
 cd ../..
 sudo rm -r temp/
